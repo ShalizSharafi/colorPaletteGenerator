b38455b63aa66d73502500cb2e4a221d6afaf6b8
shalizsharafi <shalizoputh@gmail.com>
2026-06-23 20:02:34 -0700

    delete btn added

--- js/master.js
+++ js/master.js
@@ -24,7 +24,9 @@ span.forEach((val,i)=>{
        //add lock btn
        lockColor(val)
        //add lock btn
-
+       //trash button//
+       trashBtn(val)
+       //trash button//
           // circle
         if(i == 0 ) return
         addCircle(val)
@@ -102,6 +104,34 @@ function lockColor(span){
 
 }
 
+let deleteFlag = 0
+function trashBtn(span){
+       let deleteBtn = document.createElement('i')
+       deleteBtn.classList.add('deleteBtn')
+       deleteBtn.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+</svg>`
+
+       span.addEventListener('mouseenter',()=>{
+              span.appendChild(deleteBtn)
+       })
+       span.addEventListener('mouseleave',()=>{
+              deleteBtn.remove()
+       })
+      
+     deleteBtn.addEventListener('click',(e)=>{
+       e.stopPropagation()
+       let temp = span.getAttribute('data-status')
+       if(temp == 'locked') return
+       if(deleteFlag < 4){
+              span.remove()
+              deleteFlag++
+              return
+       }
+       alert('color deletion exceeded!')
+     })
+}
+
 function addCircle(span){
        let addBtn = document.createElement('span')
        addBtn.classList.add('btn')
@@ -135,6 +165,7 @@ function addCircle(span){
               addCircle(newSpan)
               refreshBtn(newSpan)
               lockColor(newSpan)
+              trashBtn(newSpan)
               flag++
               return
               }
