b4618898abce02761d8eba116349365da7a18c46
shalizsharafi <shalizoputh@gmail.com>
2026-06-24 22:20:51 -0700

    trend section added

--- js/master.js
+++ js/master.js
@@ -12,11 +12,20 @@ const add = document.querySelector('.add')
 const counter = document.querySelector('.counter')
 
 
+const addToTrend = document.querySelector('.addToTrend')
+const card = document.querySelector('.card')
+const cardSpan = document.querySelectorAll('.cardSpan')
+const pasteColor = document.querySelectorAll('.pasteColor')
+const cardColors = document.querySelectorAll('.cardColors')
+const trendSec = document.querySelector('.trend')
+const trendCounter = document.querySelector('.trendCounter')
+
+
 
 
 const rand = (max)=>Math.floor(Math.random()*max)
 const randColor = ()=>{return `rgb(${rand(256)},${rand(256)},${rand(256)})`}
-let flag = 1
+let flag = 0
 span.forEach((val,i)=>{
        //set color
        colorSet(val)
@@ -251,10 +260,20 @@ function copyButton(span){
               copyBtn.remove()
        })
 
-       copyBtn.addEventListener('click',()=>{
+       // copyBtn.addEventListener('click',(e)=>{
+       //        e.stopPropagation()
+       //        addToClipboard(span)
+       // })
+
+       copyBtn.addEventListener('click',(e)=>{
               e.stopPropagation()
-              addToClipboard(sapn)
+              let colorCode = span.children[0].innerText
+              navigator.clipboard.writeText(colorCode)
+              setTimeout(() => {
+                    alert(`${colorCode} coppied to the clipboard`) 
+              }, 100);
        })
+      
        
 }
 ///copy button
@@ -276,9 +295,117 @@ popUpWall.addEventListener('click',()=>{
        popUpWall.style.display='none'
        popUp.style.display='none'
 })
-/////popUp
+/////popUp//////////////////////////////////////////////////////////////////////////////////////////
+//Hamburgur menu
+//Hamburgur menu
+
+/////trend secton/////////////////////////////////////////////////////////////////////////////////
+
+//////creating hex values
+       const hexValues = [1,2,3,4,5,6,7,8,9,0,'a','b','c','d','e','f']
+
+cardSpan.forEach((val,i)=>{
+        hexCodesSection(val)
+       //////add color to clipboard
+})
+      /////hex 
 
 
-/////hamburgur menu
+//        ////////////////////////generate newColorBox
 
-/////hamburgur menu
+       function hexCodesSection(cardSpan){
+              let hexCode = ''
+       for(let i =0 ; i<6 ; i++){
+              hexCode += hexValues[Math.floor(Math.random()*hexValues.length)]
+       }
+       let spanColor = hexCode
+       cardSpan.style.backgroundColor=`#${spanColor}`
+       cardSpan.children[0].innerText='#' + spanColor
+       cardSpan.children[0].classList.add('colorCode')
+       ///add color to clipboard
+       addToClipboard(cardSpan)
+       cardSpan.addEventListener('click',(e)=>{
+              let temp = e.currentTarget.children[0].innerText
+              cardSpan.parentElement.parentElement.children[1].children[0].children[0].children[0].innerText = temp
+              cardSpan.parentElement.parentElement.children[1].children[0].children[0].children[0].style.color = `#${spanColor}`
+       })
+       }
+
+////hex generator
+
+////delete from trend
+
+
+///delete from trend
+       
+       let flagCount = 0
+       addToTrend.addEventListener('click',()=>{
+              let newBox = document.createElement('div')
+              newBox.classList.add('card')
+              newBox.innerHTML = `<div class="cardColors">
+                                   <span class="cardSpan group">
+                                          <b class="colorCode group-hover:flex "></b>
+                                   </span>
+                                   <span class="cardSpan group">
+                                          <b class="colorCode group-hover:flex "></b>
+                                   </span>
+                                   <span class="cardSpan group">
+                                          <b class="colorCode group-hover:flex "></b>
+                                   </span>
+                                   <span class="cardSpan group">
+                                          <b class="colorCode group-hover:flex "></b>
+                                   </span>
+                                   <span class="cardSpan group">
+                                          <b class="colorCode group-hover:flex "></b>
+                                   </span>
+                                   <span class="cardSpan group">
+                                          <b class="colorCode group-hover:flex "></b>
+                                   </span>
+                            </div>
+                            <div class="cardDetails">
+                                   <span class="flex items-center justify-start">
+                            <a class="link pasteColor">
+                            <span class="text-blue font-bold"></span>
+                     </a> 
+                     </span>
+                                 <div class="flex items-center justify-end">
+                                    <span class="headerIcon">
+                            <img src="images/eye-line.png" alt="" class="w-1/2 h-1/2 object-cover">
+                     </span> 
+                     <span class="headerIcon">
+                            <img src="images/save-line.png" alt="" class="w-1/2 h-1/2 object-cover">
+                     </span>
+                     <span class="headerIcon deleteFromTrend">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="black" class="size-4.5">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+</svg>
+                     </span>
+                                 </div>
+                            </div>`
+              if(flagCount < 6){
+                     trendSec.appendChild(newBox)
+                     let newCardSpan = document.querySelectorAll('.card>.cardColors>.cardSpan')
+                     newCardSpan.forEach((val)=>{
+                            hexCodesSection(val)
+                     })
+                     flagCount++
+                     trendCounter.innerText = `${flagCount}/6 added`
+                     /////delete
+              let deleteFromTrend = newBox.querySelector('.deleteFromTrend')
+             
+               deleteFromTrend.addEventListener('click',()=>{
+                     newBox.remove()
+                     flagCount--
+                     trendCounter.innerText = `${flagCount}/6 added`
+              })
+                     return
+              }
+               trendCounter.innerText = null
+              trendCounter.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="blue" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+</svg>
+`
+       })
+/////trend secton
+
+
